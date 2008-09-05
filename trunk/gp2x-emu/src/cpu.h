e16b85b632b764e96d674a85f83fd244c97bf2ec
mads.elvheim <mads.elvheim@c9c259fb-4153-0410-8cd7-2f061a625708>
2008-09-05 22:59:20 +0000

    Updated cpu.h and changed the callback definitions

--- trunk/gp2x-emu/src/cpu.h
+++ trunk/gp2x-emu/src/cpu.h
@@ -2,20 +2,19 @@
 #define CPU_H_
 
 #include "registers.h"
-#include "opcodes.h"
+/*#include "opcodes.h"*/
+
+typedef unsigned long instr_t; /* needed because of circular dependancy with opcodes.h */
 
 typedef struct cpu_t
 {
     int type; /* is this the 920 or 940? MCR and MRC must know */
+    int active; /* True if the CPU is enabled, and false if it is RESET */
     /* normal registers */
     regs_t r;
     /* system control coprocessor CP15 */
+    void (*mcr_callback)(instr_t instr, cpu_t* cpu);
+    void (*mrc_callback)(instr_t instr, cpu_t* cpu);
 };
 
-    reg_t ReadRegister0(instr_t);
-    reg_t ReadRegister1(instr_t);
-    reg_t ReadRegister2(instr_t;
-    reg_t ReadRegister3(instr_t;
-    reg_t ReadRegister4(instr_t;
 #endif
-
--- trunk/gp2x-emu/src/opcodes.h
+++ trunk/gp2x-emu/src/opcodes.h
@@ -1,11 +1,13 @@
 #ifndef OPCODES_H_
 #define OPCODES_H_
+
 #include "opcodelist.h"
+#include "cpu.h"
 
 #define ARITHMETIC_S_FLAG_MASK (1<<20)
 
-typedef unsigned int instr_t;
-typedef int (*opcode_callback_t)(unsigned int, instr_t);
+typedef unsigned long instr_t;
+typedef int (*opcode_callback_t)(instr_t, cpu_t*);
 
 typedef struct opcode_mask_t
 {
@@ -14,4 +16,6 @@ typedef struct opcode_mask_t
    const char* name;
    opcode_callback_t callback;
 } opcode_mask_t;
+
+extern opcode_mask_t opcode_lut[];
 #endif
