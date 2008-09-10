1a3be08053b96369643ca3ce89cc10e6ca3df87e
fxrstor <fxrstor@c9c259fb-4153-0410-8cd7-2f061a625708>
2008-09-10 14:14:40 +0000

    new headers for arch-dependant stuff

---
+++ trunk/gp2x-emu/include/ARMsparc64.h
@@ -0,0 +1,10 @@
+#ifndef _ARMSPARC64_H_
+#define _ARMSPARC64_H_
+
+#include <arm920t.h>
+
+u32 *code_ptr;
+
+#define CHECK_CONDITION(slot) 
+
+#endif
---
+++ trunk/gp2x-emu/include/ARMx86.h
@@ -0,0 +1,69 @@
+#ifndef _ARMX86_H_
+#define _ARMX86_H_
+
+#include <arm920t.h>
+
+#define CHECK_CONDITION(slot)	{\
+			pushI32(_COND_);\
+			callRel32((u32)matchCondition-((u32)(X86CODE+5)));\
+			add32ItoR(4,ESP);\
+			and32RtoR(EAX,EAX);\
+			x86Tmp[slot]=jnzI8(0);}
+
+
+#define UPDATE_FLAG(flag,slot) {\
+			bts32I8toM(flag,&CPSR);\
+			x86SetJI8(x86Tmp[slot]);\
+			btr32I8toM(flag,&CPSR); }
+
+
+#define UPDATE_CARRY(slot) {\
+			x86Tmp[slot]=jncI8(0);\
+			UPDATE_FLAG(CARRY,slot) }
+
+#define UPDATE_ZERO(slot) {\
+			x86Tmp[slot]=jnzI8(0);\
+			UPDATE_FLAG(ZERO,slot) }
+
+#define UPDATE_OVERF(slot) {\
+			x86Tmp[slot]=jnoI8(0);\
+			UPDATE_FLAG(OVERF,slot) }
+
+#define UPDATE_SIGN(slot) {\
+			x86Tmp[slot]=jnsI8(0);\
+			UPDATE_FLAG(SIGN,slot) }
+
+
+
+#define INVERSE_UPDATE(flag,slot) {\
+			btr32I8toM(flag,&CPSR);\
+			x86SetJI8(x86Tmp[slot]);\
+			bts32I8toM(flag,&CPSR); }
+
+#define INVERSE_CARRY(slot) { \
+			x86Tmp[slot]=jncI8(0);\
+			INVERSE_UPDATE(CARRY,slot)}
+
+#define INVERSE_ZERO(slot) {\
+			x86Tmp[slot]=jnzI8(0);\
+			INVERSE_UPDATE(ZERO,slot)}
+
+#define INVERSE_SIGN(slot) {\
+			x86Tmp[slot]=jnsI8(0);\
+			INVERSE_UPDATE(SIGN,slot)}
+
+#define INVERSE_OVERF(slot) {\
+			x86Tmp[slot]=jnoI8(0);\
+			INVERSE_UPDATE(OVERF,slot)}
+
+#define SAVE_CARRY(reg) { setc(reg);}
+
+
+#define SAVE_ZERO(reg) { setz(reg);}
+
+
+#define SAVE_SIGN(reg) {sets(reg);}
+
+#define SAVE_OVERF(reg) { seto(reg);}
+
+#endif
