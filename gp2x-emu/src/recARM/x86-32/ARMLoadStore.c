b1325e57de4df54a6defa67a4c7d587ad7e6276a
fxrstor <fxrstor@c9c259fb-4153-0410-8cd7-2f061a625708>
2008-09-17 22:11:36 +0000

    implemented a lot of missing function-bodies

--- gp2x-emu/src/recARM/x86-32/ARMLoadStore.c
+++ gp2x-emu/src/recARM/x86-32/ARMLoadStore.c
@@ -98,11 +98,11 @@ void recLDM3(void){
 
 void recLDR(void){
 
-	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+	CHECK_CONDITION(0);
 
 	mov32MtoR(&GPR(_Rm_),ECX);
-	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
 	sub32RfmM(ECX,&GPR(_Rd_));
 	CALLFUNC(readMemU32);
 	mov32RtoM(EAX,&GPR(_Rd_));
@@ -111,11 +111,12 @@ void recLDR(void){
 }
 void recLDRu(void){
 
+	DEBUG_PRINT();
 	CHECK_CONDITION(0);
 
 	mov32MtoR(&GPR(_Rm_),ECX);
-	push32M(&GPR(_Rd_));
-	add32RtoM(ECX,&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	add32RtoM(ECX,&GPR(_Rn_));
 	CALLFUNC(readMemU32);
 	mov32RtoM(EAX,&GPR(_Rd_));
 
@@ -128,8 +129,8 @@ void recLDRp(void){
 	DEBUG_PRINT();
 
 	mov32MtoR(&GPR(_Rm_),ECX);
-	sub32RfmM(ECX,&GPR(_Rd_));
-	push32M(&GPR(_Rd_));
+	sub32RfmM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
 	CALLFUNC(readMemU32);
 	mov32RtoM(EAX,&GPR(_Rd_));
 
@@ -141,8 +142,8 @@ void recLDRup(void){
 	DEBUG_PRINT();
 
 	mov32MtoR(&GPR(_Rm_),ECX);
-	add32RtoM(ECX,&GPR(_Rd_));
-	push32M(&GPR(_Rd_));
+	add32RtoM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
 	CALLFUNC(readMemU32);
 	mov32RtoM(EAX,&GPR(_Rd_));
 
@@ -156,9 +157,9 @@ void recLDRi(void){
 	DEBUG_PRINT();
 
 	mov32MtoR(&GPR(_Rm_),EAX);
-	sub32RfmM(EAX,&GPR(_Rd_));
+	sub32RfmM(EAX,&GPR(_Rn_));
 	shiftDispatch[(CINST>>4)&0x7](EAX);
-	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
 	CALLFUNC(readMemU32);
 	mov32RtoM(EAX,&GPR(_Rd_));
 
@@ -173,8 +174,8 @@ void recLDRiu(void){
 
 	mov32MtoR(&GPR(_Rm_),EAX);
 	shiftDispatch[(CINST>>4)&0x7](EAX);
-	push32M(&GPR(_Rd_));
-	add32RtoM(EAX,&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	add32RtoM(EAX,&GPR(_Rn_));
 	CALLFUNC(readMemU32);
 	mov32RtoM(EAX,&GPR(_Rd_));
 
@@ -187,9 +188,9 @@ void recLDRip(void){
 	DEBUG_PRINT();
 
 	mov32MtoR(&GPR(_Rm_),EAX);
-	sub32RfmM(EAX,&GPR(_Rd_));
+	sub32RfmM(EAX,&GPR(_Rn_));
 	shiftDispatch[(CINST>>4)&0x7](EAX);
-	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
 	CALLFUNC(readMemU32);
 	mov32RtoM(EAX,&GPR(_Rd_));
 
@@ -201,198 +202,839 @@ void recLDRiup(void){
 	DEBUG_PRINT();
 
 	mov32MtoR(&GPR(_Rm_),EAX);
-	add32RtoM(EAX,&GPR(_Rd_));
+	add32RtoM(EAX,&GPR(_Rn_));
 	shiftDispatch[(CINST>>4)&0x7](EAX);
-	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
 	CALLFUNC(readMemU32);
 	mov32RtoM(EAX,&GPR(_Rd_));
 
 	x86SetJI8(x86Tmp[0]);
 }
 
+
 void recSTR(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	sub32RfmM(ECX,&GPR(_Rn_));
+	CALLFUNC(writeMemU32);
+
+	x86SetJI8(x86Tmp[0]);
 }
+
+
 void recSTRu(void){
+
 	DEBUG_PRINT();
+	CHECK_CONDITION(0);
+
+	mov32MtoR(&(GPR(_Rm_), ECX));
+	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	add32RtoM(ECX,&GPR(_Rn_));
+	CALLFUNC(writeMemU32);
+	x86SetJI8(x86Tmp[0]);
 }
+
+
+
 void recSTRp(void){
+
 	DEBUG_PRINT();
+	CHECK_CONDITION(0);
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	sub32RfmM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(writeMemU32);
+
+	x86SetJI8(x86Tmp[0]);
 }
+
+
+
 void recSTRup(void){
 	DEBUG_PRINT();
+	CHECK_CONDITION(0);
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	add32RtoM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(writeMemU32);
+
+	x86SetJI8(x86Tmp[0]);
+
 }
 
 void recSTRi(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	push32M(&GPR(_Rd_));
+	mov32MtoR(&GPR(_Rm_),EAX);
+	push32M(&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	CALLFUNC(writeMemU32);
+
+	x86SetJI8(x86Tmp[0]);
+
 }
 void recSTRiu(void){
+
+	CHECK_CONDITION(0)
 	DEBUG_PRINT();
+
+	push32M(&GPR(_Rd_))
+	mov32MtoR(&GPR(_Rm_),EAX);
+	push32M(&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	add32RtoM(EAX,&GPR(_Rn_));
+	CALLFUNC(writeMemU32);
+	
+	x86SetJI8(x86Tmp[0]);
 }
+
+
 void recSTRip(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	push32M(&GPR(_Rd_));
+	mov32MtoR(&GPR(_Rm_),EAX);
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(writeMemU32);
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recSTRiup(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	push32M(&GPR(_Rd_));
+	mov32MtoR(&GPR(_Rm_),EAX);
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	add32RtoM(EAX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(writeMemU32);
+
+	x86SetJI8(x86Tmp);
 }
 
 
 void recLDRB(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	push32M(&GPR(_Rn_));
+	sub32RfmM(ECX,&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movzxR8toR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recLDRBu(void){
+
 	DEBUG_PRINT();
+	CHECK_CONDITION(0);
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	push32M(&GPR(_Rn_));
+	add32RtoM(ECX,&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movzxR8toR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRBp(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	sub32RfmM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movzxR8toR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRBup(void){
 
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	add32RtoM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movzxR8toR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 
 void recLDRBi(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movzx32R8toR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recLDRBiu(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	push32M(&GPR(_Rn_));
+	add32RtoM(EAX,&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movzxR8toR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recLDRBip(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movzxR8toR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recLDRBiup(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	add32RtoM(EAX,&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movzxR8toR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 
 void recSTRB(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	sub32RfmM(ECX,&GPR(_Rn_));
+	CALLFUNC(writeMemU8);
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recSTRBu(void){
+
 	DEBUG_PRINT();
+	CHECK_CONDITION(0);
+
+	mov32MtoR(&(GPR(_Rm_), ECX));
+	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	add32RtoM(ECX,&GPR(_Rn_));
+	CALLFUNC(writeMemU8);
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recSTRBp(void){
+
 	DEBUG_PRINT();
+	CHECK_CONDITION(0);
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	sub32RfmM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(writeMemU8);
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recSTRBup(void){
 	DEBUG_PRINT();
+	CHECK_CONDITION(0);
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	add32RtoM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(writeMemU8);
+
+	x86SetJI8(x86Tmp[0]);
 }
 
 void recSTRBi(void){
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	push32M(&GPR(_Rd_));
+	mov32MtoR(&GPR(_Rm_),EAX);
+	push32M(&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	CALLFUNC(writeMemU8);
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recSTRBiu(void){
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	mov32MtoR(&GPR(_Rm_),EAX);
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	add32RtoM(EAX,&GPR(_Rn_));
+	CALLFUNC(writeMemU8);
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recSTRBip(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	push32M(&GPR(_Rd_));
+	mov32MtoR(&GPR(_Rm_),EAX);
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(writeMemU8);
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recSTRBiup(void){
-	DEBUG_PRINT();
-}
 
+	CHECK_CONDITION(0);
+	DEBUG_PRINT();
+
+	push32M(&GPR(_Rd_));
+	mov32MtoR(&GPR(_Rm_),EAX);
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	add32RtoM(EAX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(writeMemU8);
+
+	x86SetJI8(x86Tmp);
+}
 
 void recLDRH(void){
+
 	DEBUG_PRINT();
+	CHECK_CONDITION(0);
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	push32M(&GPR(_Rn_));
+	sub32RfmM(ECX,&GPR(_Rd_));
+	CALLFUNC(readMemU16);
+	movzxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRHu(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	push32M(&GPR(_Rn_));
+	sub32RfmM(ECX,&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movzxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRHp(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	sub32RfmM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movzxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRHup(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	add32RtoM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movzxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 
 void recLDRHi(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movzxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRHiu(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	push32M(&GPR(_Rn_));
+	add32RtoM(EAX,&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movzxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRHip(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movzxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRHiup(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	add32RtoM(EAX,&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movzxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 
 void recSTRH(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	sub32RfmM(ECX,&GPR(_Rn_));
+	CALLFUNC(writeMemU16);
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recSTRHu(void){
+
 	DEBUG_PRINT();
+	CHECK_CONDITION(0);
+
+	mov32MtoR(&(GPR(_Rm_), ECX));
+	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	add32RtoM(ECX,&GPR(_Rn_));
+	CALLFUNC(writeMemU16);
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recSTRHp(void){
+
 	DEBUG_PRINT();
+	CHECK_CONDITION(0);
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	sub32RfmM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(writeMemU16);
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recSTRHup(void){
+
 	DEBUG_PRINT();
+	CHECK_CONDITION(0);
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	add32RtoM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rd_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(writeMemU16);
+
+	x86SetJI8(x86Tmp[0]);
 }
 
 void recSTRHi(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	push32M(&GPR(_Rd_));
+	mov32MtoR(&GPR(_Rm_),EAX);
+	push32M(&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	CALLFUNC(writeMemU32);
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recSTRHiu(void){
+
+	CHECK_CONDITION(0)
 	DEBUG_PRINT();
+
+	push32M(&GPR(_Rd_))
+	mov32MtoR(&GPR(_Rm_),EAX);
+	push32M(&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	add32RtoM(EAX,&GPR(_Rn_));
+	CALLFUNC(writeMemU16);
+	
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recSTRHip(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	push32M(&GPR(_Rd_));
+	mov32MtoR(&GPR(_Rm_),EAX);
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(writeMemU16);
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recSTRHiup(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	push32M(&GPR(_Rd_));
+	mov32MtoR(&GPR(_Rm_),EAX);
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	add32RtoM(EAX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(writeMemU16);
+
+	x86SetJI8(x86Tmp);
 }
-
-
+}
 
 void recLDRSB(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	push32M(&GPR(_Rn_));
+	sub32RfmM(ECX,&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movsxR8toR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRSBu(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	push32M(&GPR(_Rn_));
+	add32RtoM(ECX,&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movsxR8ToR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
+
 }
+
 void recLDRSBp(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	sub32RfmM(ECX,&GPR(_Rn_));
+	push32(_Rn_);
+	CALLFUNC(readMemU8);
+	movsxR8ToR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
+
 }
 void recLDRSBup(void){
-	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	add32RtoM(ECX,&GPR(_Rn_));
+	push32(_Rn_);
+	CALLFUNC(readMemU8);
+	movsxR8ToR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 
 void recLDRSBi(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	push32M(&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movsxR8toR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recLDRSBiu(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	push32M(&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	add32RtoM(EAX,&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movsxR8toR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recLDRSBip(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movsxR8toR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRSBiup(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	add32RtoM(EAX,&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU8);
+	movsxR8toR32(AL,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 
 void recLDRSH(void){
+
 	DEBUG_PRINT();
+	CHECK_CONDITION(0);
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	push32M(&GPR(_Rn_));
+	sub32RfmM(ECX,&GPR(_Rd_));
+	CALLFUNC(readMemU16);
+	movsxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recLDRSHu(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	push32M(&GPR(_Rn_));
+	add32RfmM(ECX,&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movsxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRSHp(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	sub32RfmM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movsxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
+
 void recLDRSHup(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),ECX);
+	add32RtoM(ECX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movsxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 
 void recLDRSHi(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	push32M(&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movsxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRSHiu(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	push32M(&GPR(_Rn_));
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	add32RtoM(EAX,&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movsxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRSHip(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	sub32RfmM(EAX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movsxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
 void recLDRSHiup(void){
+
+	CHECK_CONDITION(0);
 	DEBUG_PRINT();
+
+	mov32MtoR(&GPR(_Rm_),EAX);
+	shiftDispatch[(CINST>>4)&0x7](EAX);
+	add32RtoM(EAX,&GPR(_Rn_));
+	push32M(&GPR(_Rn_));
+	CALLFUNC(readMemU16);
+	movsxR16toR32(AX,EAX);
+	mov32RtoM(EAX,&GPR(_Rd_));
+
+	x86SetJI8(x86Tmp[0]);
 }
